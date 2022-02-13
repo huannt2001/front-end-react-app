f7c45d174e4cc6ac6ac0cc53e666e75016f0a0fe
Huan Nguyen <nguyentranghuan638@gmail.com>
2022-02-13 15:03:17 +0700

    video 46 design header multy languages - clone Bookingcare

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './HomeHeader.scss';
+import { FormattedMessage } from 'react-intl';
 
 
 class HomeHeader extends Component {
@@ -18,20 +19,36 @@ class HomeHeader extends Component {
                         </div>
                         <ul className="list-options-content">
                             <li className="list-options-item" >
-                                <a className="sub-header">Chuyên Khoa</a>
-                                <p className="sub-title">Tìm bác sĩ theo chuyên khoa</p>
+                                <a className="sub-header">
+                                    <FormattedMessage id="homeheader.specialty" />
+                                </a>
+                                <p className="sub-title">
+                                    <FormattedMessage id="homeheader.searchdoctor" />
+                                </p>
                             </li>
                             <li className="list-options-item" >
-                                <a className="sub-header">Cơ sở y tế</a>
-                                <p className="sub-title">Chọn bệnh viện, phòng khám</p>
+                                <a className="sub-header">
+                                    <FormattedMessage id="homeheader.health-facility" />
+                                </a>
+                                <p className="sub-title">
+                                    <FormattedMessage id="homeheader.select-room" />
+                                </p>
                             </li>
                             <li className="list-options-item" >
-                                <a >Bác sĩ</a>
-                                <p>Chọn bác sĩ giỏi</p>
+                                <a className="sub-header" >
+                                    <FormattedMessage id="homeheader.doctor" />
+                                </a>
+                                <p className="sub-title">
+                                    <FormattedMessage id="homeheader.select-doctor" />
+                                </p>
                             </li>
                             <li className="list-options-item" >
-                                <a className="sub-header">Gói khám</a>
-                                <p className="sub-title">Khám sức khỏe tổng quát</p>
+                                <a className="sub-header">
+                                    <FormattedMessage id="homeheader.fee" />
+                                </a>
+                                <p className="sub-title">
+                                    <FormattedMessage id="homeheader.check-heath" />
+                                </p>
                             </li>
                         </ul>
                         <div className="right-content">
@@ -39,9 +56,12 @@ class HomeHeader extends Component {
                                 <span className="support-icon">
                                     <i className="fas fa-question-circle"></i>
                                 </span>
-                                <span>Hỗ trợ</span>
+                                <span>
+                                    <FormattedMessage id="homeheader.support" />
+                                </span>
                             </a>
-                            <div className="flag">VN</div>
+                            <div className="language-vi">VN</div>
+                            <div className="language-en">EN</div>
                         </div>
                     </div>
                 </div>
@@ -49,11 +69,10 @@ class HomeHeader extends Component {
                 <div className="home-header-banner">
                     <div className="search">
                         <div className="search-container">
-                            <h1 className="search-title">NỀN TẢNG Y TẾ
+                            <h1 className="search-title">
+                                <FormattedMessage id="banner.title1" />
                                 <br />
-                                <b>
-                                    CHĂM SÓC SỨC KHỎE TOÀN DIỆN
-                                </b>
+                                <b><FormattedMessage id="banner.title2" /></b>
                             </h1>
                             <div className="search-input-wrap">
                                 <div className="search-icon">
@@ -71,7 +90,9 @@ class HomeHeader extends Component {
                                         <div className="option-service">
                                             <i className=" fas fa-hospital"> </i>
                                         </div>
-                                        <p className="option-title">Khám Chuyên Khoa</p>
+                                        <p className="option-title">
+                                            <FormattedMessage id="banner.check-specialty" />
+                                        </p>
                                     </a>
                                 </li>
                                 <li className="option-item">
@@ -79,7 +100,9 @@ class HomeHeader extends Component {
                                         <div className="option-service">
                                             <i className=" fas fa-mobile-alt"> </i>
                                         </div>
-                                        <p className="option-title">Khám Chuyên Khoa</p>
+                                        <p className="option-title">
+                                            <FormattedMessage id="banner.remote-examination" />
+                                        </p>
                                     </a>
                                 </li>
                                 <li className="option-item">
@@ -87,39 +110,29 @@ class HomeHeader extends Component {
                                         <div className="option-service">
                                             <i className=" fas fa-procedures"> </i>
                                         </div>
-                                        <p className="option-title">Khám Chuyên Khoa</p>
+                                        <p className="option-title">
+                                            <FormattedMessage id="banner.general-examination" />
+                                        </p>
                                     </a>
                                 </li>
                                 <li className="option-item">
                                     <a className="option-item-link">
                                         <div className="option-service">
-                                            <i className=" fas fa-microscope"> </i>
+                                            <i className=" fas fa-flask"> </i>
                                         </div>
-                                        <p className="option-title">Khám Chuyên Khoa</p>
+                                        <p className="option-title">
+                                            <FormattedMessage id="banner.medical-test" />
+                                        </p>
                                     </a>
                                 </li>
                                 <li className="option-item">
                                     <a className="option-item-link">
                                         <div className="option-service">
-                                            <i className=" fas fa-tooth"> </i>
+                                            <i className=" fas fa-user-md"> </i>
                                         </div>
-                                        <p className="option-title">Khám Chuyên Khoa</p>
-                                    </a>
-                                </li>
-                                <li className="option-item">
-                                    <a className="option-item-link">
-                                        <div className="option-service">
-                                            <i className=" fas fa-hospital"> </i>
-                                        </div>
-                                        <p className="option-title">Khám Chuyên Khoa</p>
-                                    </a>
-                                </li>
-                                <li className="option-item">
-                                    <a className="option-item-link">
-                                        <div className="option-service">
-                                            <i className=" fas fa-hospital"> </i>
-                                        </div>
-                                        <p className="option-title">Khám Chuyên Khoa</p>
+                                        <p className="option-title">
+                                            <FormattedMessage id="banner.mental-health" />
+                                        </p>
                                     </a>
                                 </li>
                                 <li className="option-item">
@@ -127,7 +140,29 @@ class HomeHeader extends Component {
                                         <div className="option-service">
                                             <i className=" fas fa-hospital"> </i>
                                         </div>
-                                        <p className="option-title">Khám Chuyên Khoa</p>
+                                        <p className="option-title">
+                                            <FormattedMessage id="banner.dental-examination" />
+                                        </p>
+                                    </a>
+                                </li>
+                                <li className="option-item">
+                                    <a className="option-item-link">
+                                        <div className="option-service">
+                                            <i className="fa fa-scissors"></i>
+                                        </div>
+                                        <p className="option-title">
+                                            <FormattedMessage id="banner.surgical-packet" />
+                                        </p>
+                                    </a>
+                                </li>
+                                <li className="option-item">
+                                    <a className="option-item-link">
+                                        <div className="option-service">
+                                            <i className="fa fa-truck"></i>
+                                        </div>
+                                        <p className="option-title">
+                                            <FormattedMessage id="banner.medical-product" />
+                                        </p>
                                     </a>
                                 </li>
                             </ul>
@@ -142,7 +177,8 @@ class HomeHeader extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        lang: state.app.language,
     };
 };
 
