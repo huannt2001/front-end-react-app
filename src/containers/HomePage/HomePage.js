bd3b2a5830fa301ef45313c319363fbf63c4f39a
Huan Nguyen <nguyentranghuan638@gmail.com>
2022-02-16 10:10:12 +0700

    video 49 customize react-slider-carousel

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
-
+import Specialty from './Section/Specialty'
 class HomePage extends Component {
 
     render() {
         return (
-            <HomeHeader />
+            <div>
+                <HomeHeader />
+                <Specialty />
+            </div>
         );
     }
 
